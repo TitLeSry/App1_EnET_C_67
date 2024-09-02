f7b08bb0d32d6a7dda6cd01229e6abda81850fd5
TitLeSry <sarayut.666tle@gmail.com>
2024-09-02 11:17:20 +0700

    context-header2.js, context-content2.js, context.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Header, Content, Footer } from './func-components'
-import Calendar from './class-components'
+import { userContext } from './context'
+import Header2 from './context-header2'
+import Content2 from './context-content2'
 
 export default function App(){
+  let [user, setUser]=React.useState('')
   return (
-    <>
-      <Header/>
-      <p><center><Calendar/></center></p>
-      <Content/>
-      <Footer/>
-    </>
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
   )
 }
